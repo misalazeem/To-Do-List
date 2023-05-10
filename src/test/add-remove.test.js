45e827d9d5d7f6ebbeb06515cf7eaad3b1418624
berylcl <gakiiberyl@gmail.com>
2023-05-10 14:03:33 +0100

    Adding test functions

--- src/test/add-remove.test.js
+++ src/test/add-remove.test.js
@@ -8,4 +8,14 @@ describe('And and Remove Functionality', () => {
     clear: jest.fn(),
   }
   global.localStorage = MockStorage;
+  test('add task', () => {
+    tasklist.addTask('test task', false, tasklist.length + 1);
+    expect(tasklist).toEqual({"tasklist": [{ description: 'test task', completed: false, index: 1 }]});
+    expect(global.localStorage.setItem).toBeCalledWith('tasklists', JSON.stringify(tasklist.tasklist));
+  });
+  test('remove task', () => {
+    tasklist.removeTask(0);
+    expect(tasklist).toEqual({"tasklist": []});
+    expect(global.localStorage.setItem).toBeCalledWith('tasklists', JSON.stringify(tasklist.tasklist));
+  });
 });
